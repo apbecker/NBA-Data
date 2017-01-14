c8fef6fba2e16022ae90e034a7605714ec65eb54
clint-richardson <clint.a.richardson@gmail.com>
2017-01-14 17:22:58 -0500

    added ROOT based functions

--- DataTools/CompositeFunctions.py
+++ DataTools/CompositeFunctions.py
@@ -2,20 +2,57 @@
 
 import pandas as pd
 import numpy as np
+from ROOT import TH1F,TGraphAsymmErrors
+
+def getPlayerShotDistHist(df,player):
+    shots = df[(df['player_name']==player)].SHOT_DIST.values
+    hist = TH1F("hist","hist",50,0,50)
+    for shot in shots:
+        hist.Fill(shot)
+    return hist
+
+def makeShotAccGraph(df,player):
+    shotsmade = df[ (df['player_name']==player) & ( df['SHOT_RESULT']=='made') ].SHOT_DIST.values
+    shots = df[ (df['player_name']==player)].SHOT_DIST.values
+    numhist = TH1F("numhist","Accuracy vs. Shot Distance",25,0,50)
+    denhist = TH1F("denhist","Accuracy vs. Shot Distance",25,0,50)
+    for shot in shotsmade:
+        numhist.Fill(shot)
+    for shot in shots:
+        denhist.Fill(shot)
+    graph = TGraphAsymmErrors(numhist,denhist)
+    return graph
+
+def makeShotAccGraphTotal(df):
+    shotsmade = df[ ( df['SHOT_RESULT']=='made') ].SHOT_DIST.values
+    shots = df.SHOT_DIST.values
+    numhist = TH1F("numhist","Accuracy vs. Shot Distance",25,0,50)
+    denhist = TH1F("denhist","Accuracy vs. Shot Distance",25,0,50)
+    for shot in shotsmade:
+        numhist.Fill(shot)
+    for shot in shots:
+        denhist.Fill(shot)
+    graph = TGraphAsymmErrors(numhist,denhist)
+    return graph
 
 def getPlayerAccuracy(df,player):
     nmade = len(df[ (df['player_name']==player) & ( df['SHOT_RESULT']=='made') ])
     ntot = len(df[ (df['player_name']==player)])
-    accuracy = float(nmade)/ float(ntot)
+    accuracy = float(nmade)/ float(ntot)    
     return accuracy
 
-def getListOfPlayers(df):
 
+def getListOfPlayers(df):
     names = df['player_name'].unique()
     return names
 
+#def makePlayerTeamDict(df):
+#    names = getListOfPlayers(df)
+    #now need to loop through original and find via the matchup the common team for more than one matchup for a given player and take this team as the one for which they play
+ #   playerDF = df[
+
 def getPlayerAccuracyDF(df):
-    namesdf = df['player_name'].unique()
+    namesdf = getListOfPlayers(df)
     acc = []
     dists=[]
     shots=[]
@@ -29,5 +66,18 @@ def getPlayerAccuracyDF(df):
     retDF['AVG_SHOT_DIST']=dists
     retDF['ACCURACY']=acc
     retDF['N_SHOTS']=shots
-    print retDF.head()
+#    print retDF.head()
     return retDF
+
+
+def makeShotArrays(df,player):
+    shotsmade = df[ (df['player_name']==player) & ( df['SHOT_RESULT']=='made') ].SHOT_DIST.values
+    shots = df[ (df['player_name']==player)].SHOT_DIST.values
+
+
+
+    
+
+
+
+
